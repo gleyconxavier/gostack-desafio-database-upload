2d5db71911e2a8a9bb0acc3c320bd88db131e687
gleyconxavier <gleyconxcarlos@gmail.com>
2020-08-23 01:04:32 -0400

    Desafio completado

---
+++ src/database/migrations/1598142068804-CreateForeignkeyInTransactionsTable.ts
@@ -0,0 +1,25 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey
+} from "typeorm";
+
+export default class CreateForeignkeyInTransactionsTable1598142068804 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionsCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionsCategory');
+  }
+}
