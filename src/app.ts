af43900287ced2d785a8edb11853de9911545c92
Gleycon Xavier <gleyconxcarlos@gmail.com>
2020-08-30 12:14:49 -0400

    Fixed a typo in category model, added relation structure

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,9 @@ import createConnection from './database';
 createConnection();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
